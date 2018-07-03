255695bc235ef8a8fe7f46670857a90c07a77d0d
Alex Tough <alextough@AlexToughLaptop.local>
2018-07-03 12:32:11 -0400

    FunctionSignature implemented for a given list of inputs

--- examples/7_plus/smtcond.rkt
+++ examples/7_plus/smtcond.rkt
@@ -9,16 +9,23 @@
 ;(displayln (SMTCond (list '(and (x > 0) (y > 0)) '(+ x y))))
 ;(displayln (SMTCond (list '(and (x > 0) (y > 0)) '(+ x y) '(or (x < 5) (= y 3)) '(* x 2) '(< x y) 6)))
 
-(define (SMTCondFunction l)
-  (define (FunctionSignature l)
-    (list 'define-fun 'smtcond 'some-list-of-tuples 'Int))
+(define (SMTCondFunction input-list l)
+  (define (ParamTypeList param)
+    (match param
+      [(list name type) (list (list name type))]
+      [(cons name1 (cons type1 rest-of-params))
+       (cons (list name1 type1) (ParamTypeList rest-of-params))]))
+  (define (FunctionSignature input-list)
+    (let ([ret_type (car input-list)]
+          [param (cdr input-list)])
+     (list 'define-fun 'smtcond (ParamTypeList param) ret_type)))
   (define (Body l)
     (match l
     [(list pc result) (list 'ite pc result -1)]
     [(cons pc1 (cons result1 l)) (list 'ite pc1
                                              result1
                                              (Body l))]))
-  (append (FunctionSignature l) (list (Body l))))
+  (append (FunctionSignature input-list) (list (Body l))))
 
-(displayln (SMTCondFunction (list '(and (x > 0) (y > 0)) '(+ x y))))
-(displayln (SMTCondFunction (list '(and (x > 0) (y > 0)) '(+ x y) '(or (x < 5) (= y 3)) '(* x 2) '(< x y) 6)))
+(displayln (SMTCondFunction (list 'Int 'x 'Int) (list '(and (x > 0) (y > 0)) '(+ x y))))
+(displayln (SMTCondFunction (list 'Int 'x 'Int 'y 'Int) (list '(and (x > 0) (y > 0)) '(+ x y) '(or (x < 5) (= y 3)) '(* x 2) '(< x y) 6)))
